6182899f55a5934dbd49bf09a25ec4a9d695db99
simon <sbmp2000@gmail.com>
2025-05-01 18:01:31 -0700

    added checklist

--- str_to_num.c
+++ str_to_num.c
@@ -1,1 +1,19 @@
 //simon parker
+
+/*
+include:
+  -flags to set what type of number, -o octal -x hex -d decimal. if no flags provided, prompt the user?
+  -help flag, -h. verbose mode? what would that do
+  -error handling. user inputs invalid string? invalid combination of flags?
+  -pipe input/output to files. -r filename to read, else stdin. -o filename to output, else stdout
+  -maybe a bash program to prompt the user for several lines of input. it would then overwrite the input file if it exists and then
+    translate the file, outputting to wherever
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+  
+  return EXIT_SUCCESS
+}
